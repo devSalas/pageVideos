97ea96e75dba18edb21ee18aa6133c7d926b0060
esteban salas <esalassulca@gmail.com>
2022-07-11 22:36:24 -0500

    agrege  inicio de sesion

--- js/ValidarRegistro.js
+++ js/ValidarRegistro.js
@@ -4,16 +4,24 @@ const d=document;
 const ValidarRegistro =()=>{
   const $Form= d.querySelector(".content-form")
   const $pageVideos=d.querySelector(".pageVideos")
-  const $login = d.querySelector(".login")
-/*   const $Sign = d.querySelector(".SignUp") */
+  const $login = d.querySelector(".SignUp")
+  const $SignIn = d.querySelector(".signIn")
+  /* esto va ser para inicar sesion */
+  const $logIn=d.querySelector(".logIn")
+  const $FormLogIn=d.querySelector(".logIn-form")
   d.addEventListener("submit",(e)=>{
     e.preventDefault();
     console.log(e)
     if(e.target ===$Form){
-      $login.classList.add("none")
+      $SignIn.classList.add("none")
+      $logIn.classList.remove("none")
+    }
+    if(e.target===$FormLogIn){
+      $logIn.classList.add("none")
       $pageVideos.classList.remove("none")
       personalizarPage()
     }
+    
   })
 
 }
